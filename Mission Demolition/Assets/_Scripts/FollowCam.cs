07946ed4dac6f3cb12ecdeb7f829b3df6423ae77
Seth Kirby <slkirby22@gmail.com>
2024-10-08 18:39:14 -0400

    Completed Prototype - Tuesday October 8
    Finished prototype with 5 levels.

--- Mission Demolition/Assets/_Scripts/FollowCam.cs
+++ Mission Demolition/Assets/_Scripts/FollowCam.cs
@@ -4,17 +4,23 @@ using UnityEngine;
 
 public class FollowCam : MonoBehaviour
 {
+    static private FollowCam S;
     static public GameObject POI;
+
+    public enum eView { none, slingshot, castle, both };
 
     [Header("Inscribed")]
     public float easing = 0.05f;
     public Vector2 minXY = Vector2.zero;
+    public GameObject viewBothGO;
 
     [Header("Dynamic")]
     public float camZ;
+    public eView nextView = eView.slingshot;
 
     void Awake()
     {
+        S = this;
         camZ = this.transform.position.z;
     }
 
@@ -44,5 +50,37 @@ public class FollowCam : MonoBehaviour
         Camera.main.orthographicSize = destination.y + 10;
     }
 
+    public void SwitchView(eView newView)
+    {
+        if (newView == eView.none)
+        {
+            newView = nextView;
+        }
+        switch (newView)
+        {
+            case eView.slingshot:
+                POI = null;
+                nextView = eView.castle;
+                break;
+            case eView.castle:
+                POI = MissionDemolition.GET_CASTLE();
+                nextView = eView.both;
+                break;
+            case eView.both:
+                POI = viewBothGO;
+                nextView = eView.slingshot;
+                break;
+        }
+    }
+
+    public void SwitchView()
+    {
+        SwitchView(eView.none);
+    }
+
+    static public void SWITCH_VIEW(eView newView)
+    {
+        S.SwitchView(newView);
+    }
 
 }
--- Mission Demolition/Assets/_Scripts/MissionDemolition.cs
+++ Mission Demolition/Assets/_Scripts/MissionDemolition.cs
@@ -57,6 +57,8 @@ public class MissionDemolition : MonoBehaviour
 
         UpdateGUI();
         mode = GameMode.playing;
+
+        FollowCam.SWITCH_VIEW(FollowCam.eView.both);
     }
 
     void UpdateGUI()
@@ -73,6 +75,8 @@ public class MissionDemolition : MonoBehaviour
         if ((mode==GameMode.playing) && Goal.goalMet)
         {
             mode = GameMode.levelEnd;
+
+            FollowCam.SWITCH_VIEW(FollowCam.eView.both);
 
             Invoke("NextLevel", 2f);
         }
